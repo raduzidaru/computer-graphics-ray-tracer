fc5e869689305fbf0ac75e673e1636c0a958113a
Radu Zidaru <R.A.Zidaru@student.tudelft.nl>
2025-01-12 23:21:41 +0100

    Reflections, Transparency, BVH and Interpolation

--- src/bvh.cpp
+++ src/bvh.cpp
@@ -1,4 +1,179 @@
 #include "bvh.h"
+#include <stack>
+#include "scene.h"
+#include "render.h"
+#include "intersect.h"
+#include "interpolate.h"
+#include "draw.h"
+
+bool intersectRayWithAABB(const Ray& ray, const AxisAlignedBox& aabb)
+{
+    float t_min = 0.0f;
+    float t_max = ray.t;
+    for (int axis = 0; axis < 3; ++axis) {
+        float lower = aabb.lower[axis];
+        float upper = aabb.upper[axis];
+        float origin = ray.origin[axis];
+        float direction = ray.direction[axis];
+        if (direction == 0.0f) {
+            if (origin < lower || origin > upper) {
+                return false;
+            }
+            continue;
+        }
+        float t1 = (lower - origin) / direction;
+        float t2 = (upper - origin) / direction;
+        if (t1 > t2) {
+            std::swap(t1, t2);
+        }
+        t_min = std::max(t_min, t1);
+        t_max = std::min(t_max, t2);
+        if (t_min > t_max) {
+            return false;
+        }
+    }
+    return true;
+}
+
+glm::vec3 intersectionPointRayWithAABB(const Ray& ray, const AxisAlignedBox& aabb)
+{
+    float t_min = 0.0f;
+    float t_max = ray.t;
+    glm::vec3 intersectionPoint = glm::vec3(std::numeric_limits<float>::max());
+    for (int axis = 0; axis < 3; ++axis) {
+        float lower = aabb.lower[axis];
+        float upper = aabb.upper[axis];
+        float origin = ray.origin[axis];
+        float direction = ray.direction[axis];
+        if (direction == 0.0f) {
+            if (origin < lower || origin > upper) {
+                return glm::vec3(std::numeric_limits<float>::max());
+            }
+            continue;
+        }
+        float t1 = (lower - origin) / direction;
+        float t2 = (upper - origin) / direction;
+        if (t1 > t2) {
+            std::swap(t1, t2);
+        }
+        t_min = std::max(t_min, t1);
+        t_max = std::min(t_max, t2);
+        if (t_min > t_max) {
+            return glm::vec3(std::numeric_limits<float>::max());
+        }
+    }
+    intersectionPoint = ray.origin + ray.direction * t_min;
+    return intersectionPoint;
+}
+
+
+void updateHitInfo(RenderState& state, const Scene& scene, const BVHInterface::Primitive& primitive, const Ray& ray, HitInfo& hitInfo)
+{
+    const auto& [v0, v1, v2] = std::tie(primitive.v0, primitive.v1, primitive.v2);
+    const auto& mesh = scene.meshes[primitive.meshID];
+    glm::vec3 intersect = ray.origin + ray.direction * ray.t;
+
+    hitInfo.material = mesh.material;
+    if (state.features.enableNormalInterp) {
+        hitInfo.barycentricCoord = computeBarycentricCoord(v0.position, v1.position, v2.position, intersect);
+        hitInfo.normal = interpolateNormal(primitive.v0.normal, primitive.v1.normal, primitive.v2.normal, hitInfo.barycentricCoord);
+        hitInfo.texCoord = interpolateTexCoord(v0.texCoord, v1.texCoord, v2.texCoord, hitInfo.barycentricCoord);
+    } else {
+        const auto n = glm::normalize(glm::cross(v1.position - v0.position, v2.position - v0.position));
+        hitInfo.normal = n;
+    }
+
+    // Catch flipped normals
+    if (glm::dot(ray.direction, hitInfo.normal) > 0.0f) {
+        hitInfo.normal = -hitInfo.normal;
+    }
+}
+
+int countIntersectedAABBs(const BVHInterface& bvh, const Ray& ray)
+{
+    std::span<const BVHInterface::Node> nodes = bvh.nodes();
+    int intersectedCount = 0;
+
+    std::stack<uint32_t> stack;
+    stack.push(0);
+
+    while (!stack.empty()) {
+        uint32_t currentNodeIdx = stack.top();
+        stack.pop();
+
+        const auto& currentNode = nodes[currentNodeIdx];
+
+        if (intersectRayWithAABB(ray, currentNode.aabb)) {
+            intersectedCount++;
+
+            if (!currentNode.isLeaf()) {
+                stack.push(currentNode.leftChild());
+                stack.push(currentNode.rightChild());
+            }
+        }
+    }
+
+    return intersectedCount;
+}
+
+
+bool intersectRayWithBVHImplemented(RenderState& state, const BVHInterface& bvh, Ray& ray, HitInfo& hitInfo)
+{
+    std::span<const BVHInterface::Node> nodes = bvh.nodes();
+    std::span<const BVHInterface::Primitive> primitives = bvh.primitives();
+
+    bool is_hit = false;
+    std::stack<std::pair<uint32_t, int>> stack;
+    stack.push({ 0, 0 });
+    std::vector<int> intersectedNodes;
+
+
+    while (!stack.empty()) {
+        auto [currentNodeIdx, level] = stack.top();
+        stack.pop();
+
+        BVHInterface::Node currentNode = nodes[currentNodeIdx];
+
+        if (!intersectRayWithAABB(ray, currentNode.aabb)) {
+            continue;
+        }
+        intersectedNodes.push_back(currentNodeIdx);
+
+        if (state.features.enableDebugDraw) {
+            glm::vec3 intersectionPoint = intersectionPointRayWithAABB(ray, currentNode.aabb);
+            if (intersectionPoint.x != std::numeric_limits<float>::max()) {
+                float normalizedOrder = float(intersectedNodes.size()) / float(countIntersectedAABBs(bvh, ray));
+                glm::vec3 gradientColor = glm::mix(glm::vec3(1.0f, 1.0f, 0.0f), glm::vec3(1.0f, 0.0f, 0.0f), normalizedOrder);
+                drawSphere(intersectionPoint, 0.005f, gradientColor);
+                drawAABB(currentNode.aabb, DrawMode::Wireframe, gradientColor);
+            }
+        }
+
+        if (currentNode.isLeaf()) {
+            size_t offset = currentNode.primitiveOffset();
+            size_t count = currentNode.primitiveCount();
+
+            for (size_t i = 0; i < count; ++i) {
+                auto& prim = primitives[offset + i];
+                const auto& [v0, v1, v2] = std::tie(prim.v0, prim.v1, prim.v2);
+                if (intersectRayWithTriangle(v0.position, v1.position, v2.position, ray, hitInfo)) {
+                    is_hit = true;
+                    updateHitInfo(state, state.scene, prim, ray, hitInfo);
+                    if (state.features.enableDebugDraw) {
+                        drawTriangle(v0, v1, v2);
+                    }
+                }
+            }
+        } else {
+            stack.push({ currentNode.leftChild(), level + 1 });
+            stack.push({ currentNode.rightChild(), level + 1 });
+        }
+    }
+
+    return is_hit;
+}
+
+
 
 // TODO Standard Feature
 // Hierarchy traversal routine; you must implement this method and implement it carefully!
@@ -16,6 +191,9 @@
 // - return;   boolean, if geometry was hit or not
 bool BVH::intersect(RenderState& state, Ray& ray, HitInfo& hitInfo) const
 {
+    if (state.features.enableAccelStructure) {
+        return intersectRayWithBVHImplemented(state, *this, ray, hitInfo);
+    }
     return intersectRayWithBVH(state, *this, ray, hitInfo);
 
     // TODO: implement here your (probably stack-based) BVH traversal.
--- src/interpolate.cpp
+++ src/interpolate.cpp
@@ -1,5 +1,6 @@
 #include "interpolate.h"
 #include <glm/geometric.hpp>
+#include <iostream>
 
 // TODO Standard feature
 // Given three triangle vertices and a point on the triangle, compute the corresponding barycentric coordinates of the point.
@@ -12,8 +13,54 @@
 // This method is unit-tested, so do not change the function signature.
 glm::vec3 computeBarycentricCoord(const glm::vec3& v0, const glm::vec3& v1, const glm::vec3& v2, const glm::vec3& p)
 {
-    // TODO: implement this function.
-    return glm::vec3(0.0);
+    glm::vec3 v0v1 = v1 - v0;
+    glm::vec3 v0v2 = v2 - v0;
+    glm::vec3 v0p = p - v0;
+
+    float d00 = glm::dot(v0v2, v0v2);
+    float d01 = glm::dot(v0v2, v0v1);
+    float d11 = glm::dot(v0v1, v0v1);
+    float d20 = glm::dot(v0p, v0v2);
+    float d21 = glm::dot(v0p, v0v1);
+
+    float denom = d00 * d11 - d01 * d01;
+
+    float v = (d11 * d20 - d01 * d21) / denom;
+    float w = (d00 * d21 - d01 * d20) / denom;
+    float u = 1.0f - v - w;
+
+    if (u >= 0.0f && v >= 0.0f && w >= 0.0f && u <= 1.0f && v <= 1.0f && w <= 1.0f) {
+        return glm::vec3(u, w, v);
+    }
+
+    glm::vec3 closestPoint;
+
+    glm::vec3 edge0Closest = v0 + glm::clamp(glm::dot(v0p, v0v1) / glm::dot(v0v1, v0v1), 0.0f, 1.0f) * v0v1;
+    float dist0 = glm::length(p - edge0Closest);
+
+    glm::vec3 edge1Closest = v1 + glm::clamp(glm::dot(p - v1, v2 - v1) / glm::dot(v2 - v1, v2 - v1), 0.0f, 1.0f) * (v2 - v1);
+    float dist1 = glm::length(p - edge1Closest);
+
+    glm::vec3 edge2Closest = v2 + glm::clamp(glm::dot(p - v2, v0 - v2) / glm::dot(v0 - v2, v0 - v2), 0.0f, 1.0f) * (v0 - v2);
+    float dist2 = glm::length(p - edge2Closest);
+
+    if (dist0 < dist1 && dist0 < dist2) {
+        closestPoint = edge0Closest;
+    } else if (dist1 < dist2) {
+        closestPoint = edge1Closest;
+    } else {
+        closestPoint = edge2Closest;
+    }
+
+    glm::vec3 closestVec = closestPoint - v0;
+    d20 = glm::dot(closestVec, v0v2);
+    d21 = glm::dot(closestVec, v0v1);
+
+    v = (d11 * d20 - d01 * d21) / denom;
+    w = (d00 * d21 - d01 * d20) / denom;
+    u = 1.0f - v - w;
+
+    return glm::vec3(u, w, v);
 }
 
 // TODO Standard feature
@@ -26,8 +73,8 @@ glm::vec3 computeBarycentricCoord(const glm::vec3& v0, const glm::vec3& v1, cons
 // This method is unit-tested, so do not change the function signature.
 glm::vec3 interpolateNormal(const glm::vec3& n0, const glm::vec3& n1, const glm::vec3& n2, const glm::vec3 bc)
 {
-    // TODO: implement this function.
-    return glm::vec3(0.0);
+    glm::vec3 interpolatedNormal = bc.x * n0 + bc.y * n1 + bc.z * n2;
+    return glm::normalize(interpolatedNormal);
 }
 
 // TODO Standard feature
@@ -40,6 +87,6 @@ glm::vec3 interpolateNormal(const glm::vec3& n0, const glm::vec3& n1, const glm:
 // This method is unit-tested, so do not change the function signature.
 glm::vec2 interpolateTexCoord(const glm::vec2& t0, const glm::vec2& t1, const glm::vec2& t2, const glm::vec3 bc)
 {
-// TODO: implement this function.
-    return glm::vec2(0.0);
+    glm::vec2 interpolatedTexCoord = bc.x * t0 + bc.y * t1 + bc.z * t2;
+    return interpolatedTexCoord;
 }
